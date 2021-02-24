41b25a9d5bda4f2d6663d4673a7dcbaf9eef8b12
juniperhaven <alexianfireflies@gmail.com>
2021-02-24 15:45:01 -0800

    added initial files

---
+++ data/pokemon-pokedex.js
@@ -0,0 +1,40 @@
+{
+	"title": "Pokemon Sorted by Pokedex Number",
+	"pokemon": [
+		{
+            "Name": "Bulbasaur",
+            "PokedexNumber": 1,
+            "Generation": 1,
+            "PrimaryType": "Grass",
+            "SecondaryType": "Poison",
+            "Category": "Seed Pokemon",
+            "Evolution": "Bulbasaur > Ivysaur > Venasaur",
+            "Legendary": "No",
+			"Image": "bulbasaur.png"
+        },
+
+        {
+            "Name": "Ivysaur",
+            "PokedexNumber": 2,
+            "Generation": 1,
+            "PrimaryType": "Grass",
+            "SecondaryType": "Poison",
+            "Category": "Seed Pokemon",
+            "Evolution": "Bulbasaur > Ivysaur > Venasaur",
+            "Legendary": "No",
+			"Image": "ivysaur.png"
+        },
+
+		{
+            "Name": "Venusaur",
+            "PokedexNumber": 3,
+            "Generation": 1,
+            "PrimaryType": "Grass",
+            "SecondaryType": "Poison",
+            "Category": "Seed Pokemon",
+            "Evolution": "Bulbasaur > Ivysaur > Venasaur",
+            "Legendary": "No",
+			"Image": "venusaur.png"
+        }
+	]
+}
